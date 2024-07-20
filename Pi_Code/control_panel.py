48e7746c7b4f707d92951385dcf12b4092455b4f
Benjamin Say <benbsay@gmail.com>
2024-07-20 13:34:28 -0700

    dashboard sends SPI - test 1

--- Pi_Code/control_panel.py
+++ Pi_Code/control_panel.py
@@ -3,7 +3,9 @@ from tkinter import ttk
 
 class cntrl_pnl:
 
-    def __init__(self):
+    def __init__(self, spi):
+
+        self.spi = spi
 
         self.window_width = 775
 
@@ -16,24 +18,43 @@ class cntrl_pnl:
 
         # CONTROL BUTTONS
 
-        self.stop_btn = ttk.Button(self.controls_fr, text='STOP', command=self.stop_btn_handler)
+        self.pause_btn = ttk.Button(self.controls_fr, text='PAUSE', command=self.pause_btn_handler)
         self.generate_btn = ttk.Button(self.controls_fr, text='GENERATE', command=self.generate_btn_handler)
         self.store_btn = ttk.Button(self.controls_fr, text='STORE', command=self.store_btn_handler)
 
         # DISPLAY
+
+        widget_height = 2
+        widget_width = 10
 
         self.num_disp_fr = ttk.Frame(self.display_fr, padding=(8, 8, 8, 8), borderwidth=2, relief='sunken', width=1000, height=1000)
 
         self.screen_reset_button = ttk.Button(self.num_disp_fr, text='reset output', command=self.update_display)
         self.num_disp_obj = Text(self.num_disp_fr, wrap='word', padx=10, pady=10, width=30)
 
-        self.power_generating_fr = ttk.Frame(self.display_fr, padding=(8, 8, 8, 8), borderwidth=2, relief='sunken', height=100)
-        self.power_generating_label = ttk.Label(self.power_generating_fr, padding=(0, 0, 0, 0), text="GENERATING (W):", justify='left', anchor='w', relief='')
-        self.power_generating_disp_obj = Text(self.power_generating_fr, wrap='word', padx=10, pady=10, width=30, height=20)
+        self.W_generating_fr = ttk.Frame(self.display_fr, padding=(8, 8, 8, 8), borderwidth=2, relief='sunken', height=100)
+        self.W_generating_label = ttk.Label(self.W_generating_fr, padding=(0, 0, 0, 0), text="GENERATING (W):", justify='left', anchor='w', relief='')
+        self.W_generating_disp_obj = Text(self.W_generating_fr, wrap='word', padx=10, pady=10, width=widget_width, height=widget_height)
 
-        self.power_using_fr = ttk.Frame(self.display_fr, padding=(8, 8, 8, 8), borderwidth=2, relief='sunken', height=100)
-        self.power_using_label = ttk.Label(self.power_using_fr, padding=(0, 0, 0, 0), text="USING (W):", justify='left', anchor='w', relief='')
-        self.power_using_disp_obj = Text(self.power_using_fr, wrap='word', padx=10, pady=10, width=30, height=20)
+        self.W_using_fr = ttk.Frame(self.display_fr, padding=(8, 8, 8, 8), borderwidth=2, relief='sunken', height=100)
+        self.W_using_label = ttk.Label(self.W_using_fr, padding=(0, 0, 0, 0), text="USING (W):", justify='left', anchor='w', relief='')
+        self.W_using_disp_obj = Text(self.W_using_fr, wrap='word', padx=10, pady=10, width=widget_width, height=widget_height)
+
+        self.V_generating_fr = ttk.Frame(self.display_fr, padding=(8, 8, 8, 8), borderwidth=2, relief='sunken', height=100)
+        self.V_generating_label = ttk.Label(self.V_generating_fr, padding=(0, 0, 0, 0), text="GENERATING (V):", justify='left', anchor='w', relief='')
+        self.V_generating_disp_obj = Text(self.V_generating_fr, wrap='word', padx=10, pady=10, width=widget_width, height=widget_height)
+
+        self.V_using_fr = ttk.Frame(self.display_fr, padding=(8, 8, 8, 8), borderwidth=2, relief='sunken', height=100)
+        self.V_using_label = ttk.Label(self.V_using_fr, padding=(0, 0, 0, 0), text="USING (V):", justify='left', anchor='w', relief='')
+        self.V_using_disp_obj = Text(self.V_using_fr, wrap='word', padx=10, pady=10, width=widget_width, height=widget_height)
+
+        self.A_generating_fr = ttk.Frame(self.display_fr, padding=(8, 8, 8, 8), borderwidth=2, relief='sunken', height=100)
+        self.A_generating_label = ttk.Label(self.A_generating_fr, padding=(0, 0, 0, 0), text="GENERATING (A):", justify='left', anchor='w', relief='')
+        self.A_generating_disp_obj = Text(self.A_generating_fr, wrap='word', padx=10, pady=10, width=widget_width, height=widget_height)
+
+        self.A_using_fr = ttk.Frame(self.display_fr, padding=(8, 8, 8, 8), borderwidth=2, relief='sunken', height=100)
+        self.A_using_label = ttk.Label(self.A_using_fr, padding=(0, 0, 0, 0), text="USING (A):", justify='left', anchor='w', relief='')
+        self.A_using_disp_obj = Text(self.A_using_fr, wrap='word', padx=10, pady=10, width=widget_width, height=widget_height)
 
         # GRID
 
@@ -43,7 +64,7 @@ class cntrl_pnl:
         self.root.rowconfigure(2, weight=1)
 
         self.controls_fr.grid(column=0, row=0, sticky=(W))
-        self.stop_btn.grid(column=0, row=0, sticky=(W))
+        self.pause_btn.grid(column=0, row=0, sticky=(W))
         self.generate_btn.grid(column=1, row=0, sticky=(W))
         self.store_btn.grid(column=2, row=0, sticky=(W))
 
@@ -52,20 +73,40 @@ class cntrl_pnl:
         self.display_fr.rowconfigure(0, weight=1)
         self.display_fr.rowconfigure(1, weight=1)
 
-        self.num_disp_fr.grid(column=0, row=0, sticky=(W, N, E, S), rowspan=2)
+        self.num_disp_fr.grid(column=0, row=0, sticky=(W, N, E, S), rowspan=6)
         self.screen_reset_button.grid(column=0, row=0, sticky=(W))
         self.num_disp_obj.grid(column=0, row=1, sticky=(W, N, E, S))
         self.num_disp_fr.rowconfigure(1, weight=1)
 
-        self.power_generating_fr.grid(column=1, row=0, sticky=(E))
-        self.power_generating_label.grid(column=0, row=0, sticky=(W))
-        self.power_generating_disp_obj.grid(column=0, row=1, sticky=(W, N, E, S))
-        self.power_generating_fr.rowconfigure(1, weight=1)
+        self.W_generating_fr.grid(column=1, row=0, sticky=(E))
+        self.W_generating_label.grid(column=0, row=0, sticky=(W))
+        self.W_generating_disp_obj.grid(column=0, row=1, sticky=(W, N, E, S))
+        self.W_generating_fr.rowconfigure(1, weight=1)
 
-        self.power_using_fr.grid(column=1, row=1, sticky=(E))
-        self.power_using_label.grid(column=0, row=0, sticky=(W))
-        self.power_using_disp_obj.grid(column=0, row=1, sticky=(W, N, E, S))
-        self.power_using_fr.rowconfigure(1, weight=1)
+        self.W_using_fr.grid(column=1, row=1, sticky=(E))
+        self.W_using_label.grid(column=0, row=0, sticky=(W))
+        self.W_using_disp_obj.grid(column=0, row=1, sticky=(W, N, E, S))
+        self.W_using_fr.rowconfigure(1, weight=1)
+
+        self.V_generating_fr.grid(column=1, row=2, sticky=(E))
+        self.V_generating_label.grid(column=0, row=0, sticky=(W))
+        self.V_generating_disp_obj.grid(column=0, row=1, sticky=(W, N, E, S))
+        self.V_generating_fr.rowconfigure(1, weight=1)
+
+        self.V_using_fr.grid(column=1, row=3, sticky=(E))
+        self.V_using_label.grid(column=0, row=0, sticky=(W))
+        self.V_using_disp_obj.grid(column=0, row=1, sticky=(W, N, E, S))
+        self.V_using_fr.rowconfigure(1, weight=1)
+
+        self.A_generating_fr.grid(column=1, row=4, sticky=(E))
+        self.A_generating_label.grid(column=0, row=0, sticky=(W))
+        self.A_generating_disp_obj.grid(column=0, row=1, sticky=(W, N, E, S))
+        self.A_generating_fr.rowconfigure(1, weight=1)
+
+        self.A_using_fr.grid(column=1, row=5, sticky=(E))
+        self.A_using_label.grid(column=0, row=0, sticky=(W))
+        self.A_using_disp_obj.grid(column=0, row=1, sticky=(W, N, E, S))
+        self.A_using_fr.rowconfigure(1, weight=1)
 
         self.num_disp_fr.columnconfigure(0, weight=1)
         self.num_disp_fr.rowconfigure(1, weight=1)
@@ -73,20 +114,24 @@ class cntrl_pnl:
     # START WINDOW
     def start_cntrl_pnl(self):
         self.root.update()
-        self.root.after_idle(self.update_display)
-        self.root.mainloop()
-
-    def update_display(self, *args):
+        #self.root.after_idle(self.update_display)
         self.num_disp_obj.delete(1.0, END)
         self.num_disp_obj.insert(1.0, "Group 21 - Bouyancy Energy Storage System (BESS)")
-        print("updated display")
-        self.root.after(1000, self.update_display)
+        self.root.mainloop()
 
-    def stop_btn_handler(self):
-        print("handling stop button")
+    def update_display(self, values, *args):
+        self.num_disp_obj.insert("end -1 chars", str(values))
+        print("updated display")
+        #self.root.after(1000, self.update_display)
+
+    def pause_btn_handler(self):
+        print("handling pause button")
+        self.spi.xfer2([0x03])
 
     def generate_btn_handler(self):
         print("handling generate button")
+        self.spi.xfer2([0x02])
 
     def store_btn_handler(self):
         print("handling store button")
+        self.spi.xfer2([0x01])
--- Pi_Code/main.py
+++ Pi_Code/main.py
@@ -1,46 +1,23 @@
 from control_panel import *
 import _thread, time, spidev
 
-#######################################################################################################################
-################################################# CONTROL PANEL CODE ##################################################
-#######################################################################################################################
-
-control = cntrl_pnl()
-_thread.start_new_thread(control.start_cntrl_pnl())
-
-#######################################################################################################################
-################################################## SPI RECIEVER CODE ##################################################
-#######################################################################################################################
-
 recieved_data_LIFO = []
 
 spi = spidev.SpiDev()
 spi.open(0,0)
 spi.max_speed_hz = 100000
 
+control = cntrl_pnl(spi)
+
 def spiRecieve17Bytes_forever():
     global recieved_data_LIFO
     while(True):
-        recieved_data = spi.xfer2([0x04] + [0x00] * 5)      
+        recieved_data = spi.xfer2([0x04] + [0x00] * 5)
         if recieved_data != [0 for i in range(6)]:
             recieved_data_LIFO.append(recieved_data)
+            control.update_display(recieved_data)
         time.sleep(0.05)
 
 _thread.start_new_thread(spiRecieve17Bytes_forever, ())
 
-#######################################################################################################################
-################################################ DISPLAY RECIEVED DATA ################################################
-#######################################################################################################################
-
-# this pops the first thing in the FIFO queue and prints it to the screen.
-# eventually it will print it on the screen of the dashboard
-
-while(True):
-    if len(recieved_data_LIFO) > 0:
-        last_in_queue = recieved_data_LIFO.pop(-1)
-        print(last_in_queue)
-        recieved_data_LIFO = []
-    else:
-        print("queue of recieved data is empty")
-    time.sleep(1)
-
+control.start_cntrl_pnl()
