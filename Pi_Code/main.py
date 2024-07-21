100602b4f7cda19f47f8945460fb30a52a1eef5d
Benjamin Say <benbsay@gmail.com>
2024-07-20 18:08:25 -0700

    clean up dashboard code

--- Pi_Code/main.py
+++ Pi_Code/main.py
@@ -2,44 +2,34 @@ from control_panel import *
 import _thread, time, spidev
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(25, GPIO.IN)
+MC_transmit_ready_pin = 25 # pin 25 will go high when the MC is ready to communicate
 
-recieved_data_LIFO = []
+GPIO.setmode(GPIO.BCM) # set pin notation to match datasheet, not pin number
+GPIO.setup(MC_transmit_ready_pin, GPIO.IN)
 
 spi = spidev.SpiDev()
 spi.open(0,0)
-spi.max_speed_hz = 100000
+spi.max_speed_hz = 10000
 
 control = cntrl_pnl(spi)
 
 def spiRecieve17Bytes_forever():
-    global recieved_data_LIFO
-    
-    while(True):
-        
-        # check if the control panel has a command to send
-        if (control.spi_request_buffer_flag):
-            spi.xfer2(control.spi_request_buffer)
-            control.spi_request_buffer = []
-            control.spi_request_buffer_flag = False
-            
-        # check if the MC is ready to transmit
-        if (GPIO.input(25) == GPIO.LOW):
-            
-            # check if the control panel has a command to send
-            if (control.spi_request_buffer_flag):
-                spi.xfer2(control.spi_request_buffer)
-                control.spi_request_buffer = []
-                control.spi_request_buffer_flag = False
-            
-            # initiate a data request
-            recieved_data = spi.xfer2([0x04] + [0x00] * 5)
-            if recieved_data != [0 for i in range(6)]:
-                recieved_data_LIFO.append(recieved_data)
-                control.update_display(recieved_data)
-                
-            #time.sleep(0.5)
+
+	while(True):
+
+		# check if the MC is ready to communicate
+		if (GPIO.input(MC_transmit_ready_pin) == GPIO.LOW):
+
+			# send new command from control panel if available
+			if (control.new_command_available()):
+				spi.xfer2(control.get_last_command())
+
+			# initiate a data request
+			recieved_data = spi.xfer2([0x04] + [0x00] * 5)
+			if recieved_data != [0 for i in range(6)]:
+				control.update_display(recieved_data)
+
+			#time.sleep(0.5)
 
 _thread.start_new_thread(spiRecieve17Bytes_forever, ())
 
